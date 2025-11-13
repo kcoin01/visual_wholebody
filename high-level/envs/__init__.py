c262f5e0a3439c6ad7e110b59db52174b856bba7
donggeon <kcoin@kaist.ac.kr>
2025-11-13 14:05:17 +0900

    ericonaldo publiccheckrollrew_37000_2 b1z1_pickmulti.py train_multistate.py added envs/__init__.py edited

--- high-level/envs/__init__.py
+++ high-level/envs/__init__.py
@@ -1,2 +1,4 @@
-from .b1z1_pickmulti import B1Z1PickMulti
-from .b1z1_pickmulti import B1Z1PickMulti as B1Z1Float
+# from .b1z1_pickmulti import B1Z1PickMulti
+# from .b1z1_pickmulti import B1Z1PickMulti as B1Z1Float
+from .b1z1_pickmulti_ericonaldo import B1Z1PickMulti
+from .b1z1_pickmulti_ericonaldo import B1Z1PickMulti as B1Z1Float
